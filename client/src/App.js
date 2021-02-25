44b1f263b51644224f021a86a37c3142fc7b1708
n0y0j <n0y0j77@gmail.com>
2021-02-25 17:06:20 +0900

    feat: Create client project using react.

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Login} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
