67a945c030e5677508fd766e9008b81b7cc86c47
n0y0j <n0y0j77@gmail.com>
2021-02-28 17:19:38 +0900

    feat: Connect MongoDB.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 const data = require('./test.json')
+const mongoose = require('mongoose')
 const { ApolloServer, gql } = require('apollo-server')
+
+mongoose.connect('mongodb+srv://admin:asd123456@ustock.tnrtb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
 
 const typeDefs = gql`
     type Query {
