8cf99083e13c9bc9f70b8bcf8dac7c8ff6b9f467
n0y0j <n0y0j77@gmail.com>
2021-03-05 21:36:24 +0900

    feat: Get s&p 500 companies data.

--- server/models/typedefs/StockType.js
+++ server/models/typedefs/StockType.js
@@ -1,47 +1,73 @@
 const { gql } = require("apollo-server");
-const puppeteer = require('puppeteer')
-const cheerio = require('cheerio');
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+const finvizor = require("finvizor");
 
 const typeDefs = gql`
   type Stock {
-    id: ID!
-    name: String!
+    tikr: String
+    name: String
+    exchange: String
+    sector: String
+    price: Float
+    change: Float
+    volume: Float
   }
 `;
+
+const getStockData = async () => {
+  var stockList = [];
+
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+
+  const page = await browser.newPage();
+  await page.goto("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies");
+  const content = await page.content();
+
+  const $ = cheerio.load(content);
+
+  const lists = $("#constituents > tbody > tr");
+
+  await Promise.all(
+    lists.map(async (index, list) => {
+      const tikr = $(list)
+        .find("td > a.external.text")
+        .text()
+        .replace("reports", "");
+
+      if (index > 0) {
+        let searchStock = await finvizor.stock(tikr);
+
+        var stock = {
+          tikr: searchStock.ticker,
+          name: searchStock.name,
+          exchange: searchStock.exchange,
+          sector: searchStock.sector,
+          price: searchStock.price,
+          change: searchStock.change,
+          volume: searchStock.volume,
+        };
+
+        if (stock.tikr !== null) {
+          stockList.push(stock);
+        }
+      }
+    })
+  );
+
+  browser.close();
+  console.log(stockList.length);
+  return stockList;
+};
 
 const resolvers = {
   Mutation: {
     searchStock: async (parent, args, context, info) => {
+      const stockList = await getStockData();
 
-      const browser = await puppeteer.launch({
-        headless: true
-      })
-
-      
-      const page = await browser.newPage();
-
-      await page.goto('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies');
-
-      const content = await page.content();
-
-      const $ = cheerio.load(content);
-
-      const lists = $("#constituents > tbody > tr")
-
-      lists.each((index, list) => {
-
-        const name = $(list).find("td > a.external.text").text()
-        const security = $(list).find("td > a").text()
-        const sector = $(list).find("td").text()
-
-        console.log({
-          index, name, security, sector
-        })
-      })
-
-      browser.close()
-
-      return true;
+      return stockList;
     },
   },
 };
