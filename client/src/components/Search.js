4751991ae2fe8be14b7bfa2164642ef6129c71bb
n0y0j <n0y0j77@gmail.com>
2021-03-10 22:04:31 +0900

    feat: Implement stock chart.

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,33 +1,66 @@
-import React, { useState } from 'react'
-import { Button } from './Button'
-import { Input } from './Input'
-import './Search.css'
+import React, { useState } from "react";
+import { Button } from "./Button";
+import { Input } from "./Input";
+import "./Search.css";
+import { TypeChooser } from "react-stockcharts/lib/helper";
+import Chart from "../components/Chart/Chart";
 
 function Search() {
+  var currentDate = new Date();
+  var temp = new Date();
+  temp.setHours(temp.getHours() + 1);
 
-    const [TIKR, setTIKR] = useState("")
+  const data = [
+    {
+      date: currentDate,
+      open: 25.455,
+      high: 27.655,
+      low: 23.1111,
+      close: 26.3384,
+      volume: 3012312,
+    },
+    {
+      date: temp,
+      open: 26.3384,
+      high: 100.655,
+      low: 19.1111,
+      close: 40.3384,
+      volume: 3012312,
+    },
+  ];
 
-    const tikrChangeHandler = (event) => {
-        setTIKR(event.currentTarget.value)
-    }
+  const [TIKR, setTIKR] = useState("");
 
-    const handleClick = (event) => {
-        event.preventDefault();
-    }
+  const tikrChangeHandler = (event) => {
+    setTIKR(event.currentTarget.value);
+  };
 
-    return (
-        <div className="main-container">
-            <div className="search-container">
-                <Input type="text" placeholder="티커를 입력해주세요" inputStyle="input-home" onChange={tikrChangeHandler} value={TIKR} />
-                <Button
-                text="검색"
-                onClick={handleClick}
-                buttonStyle="btn-home"
-                buttonSize="btn-homesize"
-              />
-            </div>
-        </div>
-    )
+  const handleClick = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="main-container">
+      <div className="search-container">
+        <Input
+          type="text"
+          placeholder="티커를 입력해주세요"
+          inputStyle="input-home"
+          onChange={tikrChangeHandler}
+          value={TIKR}
+        />
+        <Button
+          text="검색"
+          onClick={handleClick}
+          buttonStyle="btn-home"
+          buttonSize="btn-homesize"
+        />
+      </div>
+      <div className="chart-container">
+        <TypeChooser>{(type) => <Chart type={type} data={data} />}</TypeChooser>
+      </div>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import Cards from "../components/Cards";
 import Navbar from "../components/Navbar";
 import Search from "../components/Search";
-import "./Home.css";
 
 function Home() {
 
