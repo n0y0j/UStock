d97031cb22bed7f166f6c4aa7451f488693ad5b2
n0y0j <n0y0j77@gmail.com>
2021-03-25 21:45:35 +0900

    feat: Implement stock chart in Stock page.

--- client/src/pages/Stock.js
+++ client/src/pages/Stock.js
@@ -1,17 +1,23 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Navbar from "../components/Navbar";
 import Search from "../components/Search";
+import StockChart from "../components/StockChart";
 import "./Stock.css";
 
 function Stock(props) {
 
-    useEffect(() => {
-        console.log(props.location.state.tikr)
-    }, [])
   return (
     <div className="main-container">
       <Navbar />
       <Search />
+      <div className="content-container">
+        <div className="chart-container">
+          <div className="chart-title">
+            <p>{props.location.state.tikr}</p>
+          </div>
+          <StockChart tikr={props.location.state.tikr}/>
+        </div>
+      </div>
     </div>
   );
 }
