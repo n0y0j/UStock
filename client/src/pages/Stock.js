9198955005884ffd7f02a880b23df79bd2fcfafd
n0y0j <n0y0j77@gmail.com>
2021-04-06 20:21:54 +0900

    feat: Implement stock content.

--- client/src/pages/Stock.js
+++ client/src/pages/Stock.js
@@ -6,7 +6,6 @@ import StockChart from "../components/StockChart";
 import "./Stock.css";
 
 function Stock(props) {
-
   return (
     <div className="main-container">
       <Navbar />
@@ -16,9 +15,21 @@ function Stock(props) {
           <div className="chart-title">
             <p>{props.location.state.tikr}</p>
           </div>
-          <StockChart tikr={props.location.state.tikr}/>
+          <StockChart tikr={props.location.state.tikr} />
         </div>
-          <CircleProgress />
+        <div className="explain-container">
+          <h2>Apple Inc.</h2>
+          <p>NASD</p>
+          <h3>테마: Technology</h3>
+          <h3>거래량: 111,932,640</h3>
+          <h3>직원수: 147,000</h3>
+          <div className="explain-price">
+            <h4>가격: 124.76</h4>
+            <p>-0.81</p>
+            <p>0.65%</p>
+          </div>
+        </div>
+        {/* <CircleProgress /> */}
       </div>
     </div>
   );
