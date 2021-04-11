30a05ab72f7cabced2200635a548a1dda7ddbc49
n0y0j <n0y0j77@gmail.com>
2021-04-11 23:09:26 +0900

    feat: Implement parallel data insertion function.

---
+++ server/models/typedefs/worker.js
@@ -0,0 +1,38 @@
+const finvizor = require("finvizor");
+const stock = require("@ahang/stock");
+const { Stock } = require("../Stock");
+
+const process = async (arrayOfPromises) => {
+    await Promise.all(arrayOfPromises);
+}
+
+const saveStock = async (tikr) => {
+  const searchStock = await finvizor.stock(tikr);
+  const marketData = await stock.getHistoricalData(tikr);
+  const earning = await stock.getEarningData(tikr);
+  const revenue = await stock.getRevenueData(tikr);
+  const priceTarget = await stock.getPriceTargetData(tikr);
+
+  await Stock.create({
+    tikr: searchStock.ticker,
+    name: searchStock.name,
+    exchange: searchStock.exchange,
+    sector: searchStock.sector,
+    marketCap: searchStock.marketCap,
+    income: searchStock.income,
+    sales: searchStock.sales,
+    employees: searchStock.employees,
+    price: searchStock.price,
+    change: searchStock.change === null ? 0 : searchStock.change,
+    changePrice: (searchStock.price - searchStock.prevClose).toFixed(4),
+    volume: searchStock.volume,
+    analyst: {
+      earning: earning,
+      revenue: revenue,
+      priceTarget: priceTarget,
+    },
+    marketData: marketData,
+  })
+};
+
+module.exports = { saveStock, process }
